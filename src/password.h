7c78f05afc08e2eac19b7dc4f2e60be34eaece67
Shruti-sahu <102422656+Shruti-sahu23@users.noreply.github.com>
2026-06-04 12:23:45 +0530

    Add password.h header file with password functions

---
+++ src/password.h
@@ -0,0 +1,16 @@
+#ifndef _PASSWORD_H_
+#define _PASSWORD_H_
+
+#include "types.h"
+
+#define PASS_LENGTH 4
+
+void Password_Init(void);
+
+u8 Password_Verify(void);
+
+void Password_Change(void);
+
+#endif
+
+
