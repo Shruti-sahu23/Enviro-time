b4e62eba2e0ce22451cb5d12476517d86c7b66ac
Shruti-sahu <102422656+Shruti-sahu23@users.noreply.github.com>
2026-06-05 14:34:55 +0530

    Add pin connection definitions for LCD and components

--- src/pin_connections.h
+++ src/pin_connections.h
@@ -1,12 +1,21 @@
+//Hardware Pin configurations//
+
+//LCD pins
 #define LCD_DATA 8//@p0.8 to p0.15
 #define LCD_RS 16//@p0.16
 #define LCD_RW 17//@p0.17
 #define LCD_EN 18//@p0.18
 
+//Keypad row0-row3 :16-19
 #define ROW_MASK (0xF << 16)
+
+//Keypad col0-col3 :20-23
 #define COL_MASK (0xF << 20)
 
 #define BUZZER   (1<<19)
+
+//Alarm switch
 #define SW_ALARM (1<<1)
 
+//Menu switch
 #define SW1 (1 << 7)
