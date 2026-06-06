c916e5f04eca5d1a082b53d24f6576af3c874f4f
Shruti-sahu <102422656+Shruti-sahu23@users.noreply.github.com>
2026-06-06 12:29:06 +0530

    Add alarm functionality with menu and task handling

--- src/alarm.c
+++ src/alarm.c
@@ -36,19 +36,29 @@
 #include<string.h>
 #include "pin_connections.h"
 
-#define MODE_RTC      0
+//
+#define MODE_RTC  0
 
+//Indicates whether an alarm is currently active
 u8 g_alarm_active = 0;
 
+//current position used during alarm editing
 u8 pos = 0;
 
+//stores next snooze trigger time
 u32 snooze_time=0XFFFF;
+
+//indicates snooze mode status
 u8 snooze_active=0;
+
+//Counts no of snooze operations
 u8 snooze_count=0;
 
+//extern variables for alarm1 and alarm2 time checking
 extern u32 alarm1;
 extern u32 alarm2;
 
+//extern variables for storing the current mode value and prev mode
 extern u8 mode;
 extern u8 prev_mode;
 
@@ -66,14 +76,19 @@ extern s32 prev_sec;
 // --------------------------------------------------
 void Alarm_Init(void)
 {
-		PINSEL0&= ~(3<<2);
-	
-    PINSEL1&=~(3<<18);
-    
-    IODIR0 |= BUZZER;
+	//Configure buzzer pin as GPIO
+	PINSEL0&= ~(3<<2);
 
+	//Configure alarm switch pin as GPIO
+    PINSEL1&=~(3<<18);
+
+	//Set buzzer pin as output
+    IODIR0 |= BUZZER;
+	
+	//Set alarm acknowledgment switch as input 
     IODIR0 &= ~SW_ALARM;
 
+	//Ensure buzzer is OFF during startup
     IOCLR0 = BUZZER;
 }
 
@@ -102,50 +117,64 @@ void Alarm_Menu(u32 *a1, u32 *a2)
 
     while(1)
     {
+		//clear the first and second line of lcd
         ClearLineLCD(1);
         ClearLineLCD(2);
+
+		//extract alarm 1 hour and min value
         a1h=(*a1)/60;
         a1m=(*a1)%60;
-                
+
+		//extract alarm 2 hour and min value
         a2h=(*a2)/60;
         a2m=(*a2)%60;
                 
-			// Display currently configured alarm timings.
+		// Display currently configured alarm timings.
         sprintf(line1,"A1%02d:%02d A2%02d:%02d",a1h,a1m,a2h,a2m);
                 
         LCD_Print(GOTO_LINE1_POS0,line1);
             
         LCD_Print(GOTO_LINE2_POS0,"1:A1 2:A2");
 
+		//Display exit option
         LCD_Print(GOTO_LINE2_POS0+10,"3:BACK");
 
+		//wait for user to press key
         key = GetKey();
 
+		//Exit alarm menu
         if(key == '3')
         {
+			//clear LCD
             CmdLCD(CLEAR_LCD);
             return;
         }
 				
-				// User selected Alarm 1 or Alarm 2 for editing.
+		// User selected Alarm 1 or Alarm 2 for editing.
         if(key == '1' || key == '2')
         {
+			//Store selected alarm number
             char sel=key;
+
+			//Character array for storing alarm values
             char buf[6];
+
+			// Initialize alarm entry buffer
             strcpy(buf,"00:00");
             CmdLCD(CLEAR_LCD);
             LCD_Print(GOTO_LINE1_POS0,(sel=='1') ? "SET ALARM1     ": "SET ALARM     ");
 
             LCD_Print(GOTO_LINE2_POS0,buf);
             CmdLCD(GOTO_LINE2_POS0+pos);
-            CmdLCD(0X0E);
+			
+            CmdLCD(0X0E);//DSP_ON_CUR_ON
                     
             while(1)
             {
                
                 key = GetKey();
 							
-					// Move cursor forward while skipping ':' position.
+				// Move cursor forward while skipping ':' position.
                 if(key == '+')
                 {
                     if(pos < 4)
@@ -158,7 +187,7 @@ void Alarm_Menu(u32 *a1, u32 *a2)
                 CmdLCD(GOTO_LINE2_POS0+pos);
                 }
 								
-					// Move cursor backward while skipping ':' position.
+				// Move cursor backward while skipping ':' position.
                 else if(key == '-')
                 {
                     if(pos > 0)
@@ -171,13 +200,17 @@ void Alarm_Menu(u32 *a1, u32 *a2)
                  CmdLCD(GOTO_LINE2_POS0+pos);
                 }
 
-					// Reset alarm entry field to default value.
+				// Reset alarm entry field to default value.
                 else if(key == 'C')
                 {
                     strcpy(buf,"00:00");
-                                        LCD_Print(GOTO_LINE2_POS0,buf);
-                                    pos=0;
-                                    CmdLCD(GOTO_LINE2_POS0+pos);
+					
+					//Display 00:00
+                    LCD_Print(GOTO_LINE2_POS0,buf);
+
+					//Set pointer position 0
+					pos=0;
+                    CmdLCD(GOTO_LINE2_POS0+pos);
                 }
 								
 				 // Validate entered alarm time and save if valid.
@@ -186,48 +219,54 @@ void Alarm_Menu(u32 *a1, u32 *a2)
                     u8 hh;
                     u8 mm;
 
+					//Convert stored char buffer values into hours and mins
                     hh=((buf[0]-'0')*10)+(buf[1]-'0');
-
                     mm=((buf[3]-'0')*10)+(buf[4]-'0');
 										
-										// Reject invalid alarm times.
+					// Reject invalid alarm times.
                     if(hh > 23 || mm > 59)
                     {
                         CmdLCD(CLEAR_LCD);
-                        LCD_Print(GOTO_LINE1_POS0,
-                        "INVALID TIME");
+
+						//Display invalid time message
+                        LCD_Print(GOTO_LINE1_POS0,"INVALID TIME");
                         delay_ms(1000);
                         CmdLCD(CLEAR_LCD);
+
+						//Ask user to press 1 or 2 if 1 display SET ALARM1 else SET ALARM2
                         LCD_Print(GOTO_LINE1_POS0,(sel=='1')?"SET ALARM1":"SET ALARM2");
+
+						//Display 00:00
                         LCD_Print(GOTO_LINE2_POS0,buf);
                         CmdLCD(GOTO_LINE2_POS0+pos);
                         continue;
                     }
-
+					// Save Alarm1 value
                     if(sel=='1')
-                        *a1 = (hh*60)+mm;
+                        *a1 = (hh*60)+mm;	
+					// Save Alarm2 value
                     else
                         *a2 = (hh*60)+mm;
 
-                    CmdLCD(0x0C);
-
+                    CmdLCD(0x0C);//DSP_ON_CUR_OFF
                     CmdLCD(CLEAR_LCD);
-
-                    LCD_Print(GOTO_LINE1_POS0,
-                    "ALARM SAVED");
-
+                    LCD_Print(GOTO_LINE1_POS0,"ALARM SAVED");
                     delay_ms(1000);
 
                     break;
                 }
 
+				//if keys pressed between 0 to 9
                 else if(key >= '0' && key <= '9')
                 {
+					//store key pressed in char buffer
                     buf[pos] = key;
+
+					//store null value at fifth index
                     buf[5]='\0';
+					
                     LCD_Print(GOTO_LINE2_POS0,buf);
                     CmdLCD(GOTO_LINE2_POS0+pos);
-
                 }
             }
         }
@@ -255,31 +294,36 @@ void Alarm_Menu(u32 *a1, u32 *a2)
 
 void Alarm_Task(void)
 {
+	//Variables for storing hour min and sec values
     s32 hr,min,sec;
 
+	//variable used to trigger alarm only when min changes
     static u8 alarm_ack = 0;
+
+	//used to prevent LCD flickering
     static u8 alarm_screen=0;
 
+	//to store the prev alarm time
     static u32 last_alarm_time = 0xFFFF;
 
+	//current time
     u32 cur;
 
     GetRTCTimeInfo(&hr,&min,&sec);
 		
-		// Convert current time into total minutes from midnight.
-		// This simplifies alarm time comparison.
+	// Convert current time into total minutes from midnight.
+	// This simplifies alarm time comparison.
     cur = (hr*60)+min;//works for minutes snooze
 
-		// Reset alarm acknowledgement when minute changes.
+	// Reset alarm acknowledgement when minute changes.
     // Allows future alarms to trigger normally.
     if(cur != last_alarm_time)
     {
-        alarm_ack = 0;
-        
+        alarm_ack = 0;  
     }
 
-		 /*
-			Check whether:
+	/*
+		Check whether:
 
 		 1. Current time matches Alarm 1.
 		 2. Current time matches Alarm 2.
@@ -287,82 +331,93 @@ void Alarm_Task(void)
 
 		 If any condition becomes true,
 		 activate alarm state.
-		 */
+	*/
     if((g_alarm_active == 0)&&((((cur == alarm1) || (cur == alarm2))&&(alarm_ack == 0))||
 		((cur == snooze_time)&&(snooze_active)&&(sec == 0)&&(alarm_ack == 0)&&(g_alarm_active==0))))
 		{
-				g_alarm_active = 1;
-        alarm_screen=0;
-        snooze_active = 0;
-				last_alarm_time = cur;
+		
+    	// Mark alarm as active
+		g_alarm_active = 1;
 
+		// Force alarm screen refresh
+        alarm_screen=0;
+
+		// Disable snooze mode
+        snooze_active = 0;
+
+		//updating last alarm time as current time for snooze purpose
+		last_alarm_time = cur;
 		}
 
-		// Alarm active state.
-		//
-		// Displays alarm information and
-		// continuously drives buzzer output.
+	// Alarm active state.	
+	// Displays alarm information and
+	// continuously drives buzzer output.
     if(g_alarm_active)
-    {
-            
-                char abuf[17];
-                u32 atime;
-                u8 ah,am;
+    	{            
+        	char abuf[17];
+        	u32 atime;
+        	u8 ah,am;
                 
-                static u8 alarm_counter=0;
-                static s32 last_sec=-1;
-                
-                if(cur==alarm1)
-                    atime=alarm1;
-                else if(cur==alarm2)
-                    atime=alarm2;
-                else
-                    atime=snooze_time;
-                
-                ah=atime/60;
-                am=atime%60;
+        	static u8 alarm_counter=0;
+        	static s32 last_sec=-1;
+
+		//if current time is same as alarm1 time
+        if(cur==alarm1)
+            atime=alarm1;//store alarm1 time in atime variable
+        else if(cur==alarm2)//if current time is same as alarm2 time
+            atime=alarm2;//store alarm 2 time in atime variable
+        else
+            atime=snooze_time;//else store snooze time in atime variable
+
+		
+        ah=atime/60;
+        am=atime%60;
 								
-								// Turn ON buzzer while alarm remains active.
-								IOSET0 = BUZZER;
+		// Turn ON buzzer while alarm remains active.
+		IOSET0 = BUZZER;
 								
-								// Display alarm screen only once
-								// when alarm is first activated.
-                if(alarm_screen==0)
-                {
-                
-                CmdLCD(CLEAR_LCD);
-								LCD_Print(GOTO_LINE1_POS0,"**** ALARM ****");
-                
+		// Display alarm screen only once, when alarm is first activated.
+        if(alarm_screen==0)
+            {
+            	CmdLCD(CLEAR_LCD);
+				LCD_Print(GOTO_LINE1_POS0,"**** ALARM ****");
+
+				//Display alarm time in hours and mins
                 sprintf(abuf,"ALARM %02d:%02d",ah,am);
-								LCD_Print(GOTO_LINE2_POS0,abuf);
+				LCD_Print(GOTO_LINE2_POS0,abuf);
                 alarm_screen=1;
-                }
+            }
 								
-								// Count elapsed seconds while alarm is active.
-								// Used for automatic snooze timing.
-                
-								if(last_sec!=sec)
-                {
+		// Count elapsed seconds while alarm is active.
+		// Used for automatic snooze timing.
+		if(last_sec!=sec)
+            {
                 last_sec=sec;
+				// Increment alarm duration counter
                 alarm_counter++;
-                }
+            }
 								
-								// Auto Snooze:
-								//
-								// If alarm remains unattended for 10 seconds,
-								// stop buzzer and postpone alarm by 1 minute.
+		// Auto Snooze:
+		// If alarm remains unattended for 10 seconds,
+		// stop buzzer and postpone alarm by 1 minute.
                 
-								if(alarm_counter>=10)
-                {
+		if(alarm_counter>=10)
+            {
+				//make g_alarm_active as 0 to trigger the alarm again
                 g_alarm_active=0;
+
+				//reset the alarm counter
                 alarm_counter=0;
+
+				//update snooze time as current time plus 1 min to trigger the alarm after 1 min
                 snooze_time=cur+1;
 
-                if(snooze_time>=1440)
-                {
-                snooze_time-=1440;
+			// Handle day rollover
+            if(snooze_time>=1440)
+            	{
+                	snooze_time-=1440;
                 }
-                snooze_active=1;
+            	snooze_active=1;
                 snooze_count++;
                 alarm_ack=1;
                 IOCLR0=BUZZER;
@@ -372,7 +427,11 @@ void Alarm_Task(void)
                 delay_ms(500);
                 CmdLCD(CLEAR_LCD);
                 prev_sec=-1;
+				
+				//update prev mode variable as 255 to switch to the prev mode 
                 prev_mode=255;
+
+				//make mode as rtc mode to display the rtc screen
                 mode=MODE_RTC;
                 return;
                 }
@@ -383,28 +442,23 @@ void Alarm_Task(void)
 			// prevents re-triggering during current minute.					
         
 			if(IOPIN0 & SW_ALARM)
-        {
-            delay_ms(20);
+        	{
+            	delay_ms(20);
 
-            if(IOPIN0 & SW_ALARM)
-            {
-                IOCLR0 = BUZZER;
-
-                g_alarm_active = 0;
-                                alarm_screen=0;
-                            
-                alarm_ack = 1;
-
-                
-
-                CmdLCD(CLEAR_LCD);
-
-                prev_sec = -1;
-            }
-        }
-    }
+            	if(IOPIN0 & SW_ALARM)
+            	{
+                	IOCLR0 = BUZZER;
+                	g_alarm_active = 0;
+                    alarm_screen=0;
+                    alarm_ack = 1;
+                	CmdLCD(CLEAR_LCD);
+					prev_sec = -1;
+            	}
+        	}
+    	}
     else
     {
+		//turn off the buzzer
         IOCLR0 = BUZZER;
     }
 }
